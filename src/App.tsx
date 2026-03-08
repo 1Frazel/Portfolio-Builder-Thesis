47b32b0acc1103c90c4ef7c2e85a551c429aa889
azan-personal <112457939+azanra@users.noreply.github.com>
2026-03-08 14:17:15 +0700

    feat: use user input to create the pdf file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { PDFViewer } from "@react-pdf/renderer";
 import "./App.css";
+import CurrentDocument from "./pages/generate-pdf/Document";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+  });
+
   return (
-    <>
-      <div className="text-red-500 underline">Hello World!</div>
-    </>
+    <div className="h-screen w-screen flex px-[8px] py-[4px]">
+      <div className="basis-1/2">
+        <div>
+          <label htmlFor="">Full Name: </label>
+          <input
+            type="text"
+            value={user.name}
+            className="border"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          />
+        </div>
+      </div>
+      <div className="basis-1/2">
+        <PDFViewer className="w-full h-full" showToolbar={false}>
+          <CurrentDocument user={user} />
+        </PDFViewer>
+      </div>
+    </div>
   );
 }
 
---
+++ src/pages/generate-pdf/Document.tsx
@@ -0,0 +1,32 @@
+import { Document, Page, StyleSheet, Text, View } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#ffffff",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexFlow: 1,
+  },
+});
+
+const CurrentDocument = ({ user }: { user: { name: string } }) => {
+  const { name } = user;
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>{name}</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+export default CurrentDocument;
