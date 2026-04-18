dddb0972a623c68418a7b783d654118e70493c2a
azan-personal <112457939+azanra@users.noreply.github.com>
2026-04-18 10:22:15 +0700

    fix: remove duplicated entry and adjust spacing

--- src/pages/generate-pdf/components/input/SkillInput.tsx
+++ src/pages/generate-pdf/components/input/SkillInput.tsx
@@ -1,56 +1,61 @@
 const SkillInput = ({
-  handleClick,
+  defaultValue,
+  onChange,
 }: {
-  handleClick: (input: string) => void;
+  defaultValue: string;
+  onChange: (input: string) => void;
 }) => {
   const listSkills = [
     {
       value: 1,
       description: "novice",
       title: "Novice",
-      color: "#1FC8A6",
+      color: "#1fc8a6",
     },
     {
       value: 2,
       description: "basic",
       title: "Basic",
-      color: "#1FC838",
+      color: "#1fc838",
     },
     {
       value: 3,
       description: "intermediate",
       title: "Intermediate",
-      color: "#DFEB30",
+      color: "#dfeb30",
     },
     {
       value: 4,
       description: "experienced",
       title: "Experiences",
-      color: "#EB8130",
+      color: "#eb8130",
     },
     {
       value: 5,
-      description: "experienced",
-      title: "Experienced",
-      color: "#EB3030",
+      description: "expert",
+      title: "Expert",
+      color: "#eb3030",
     },
   ];
 
   return (
-    <div className="flex items-center gap-[16px]">
-      {listSkills.map((skill) => {
-        return (
-          <div key={skill.value}>
-            <div
-              onClick={() => handleClick(skill.description)}
-              className={`bg-[#${skill.color}] h-[86px] w-[48px] flex justify-center items-center`}
-            >
-              <p className="text-white">{skill.value}</p>
+    <div>
+      <p>Skills Expertise</p>
+      <div className="flex items-center gap-[16px]">
+        {listSkills.map((skill) => {
+          return (
+            <div key={skill.value} className="h-[70px] w-[70px]">
+              <div
+                onClick={() => onChange(skill.description)}
+                className={`${skill.description === defaultValue ? `bg-[${skill.color}]` : "bg-[#889290]"} flex justify-center items-center h-full`}
+              >
+                <p className="text-white">{skill.value}</p>
+              </div>
+              <p className="text-xs text-center">{skill.title}</p>
             </div>
-            <p>{skill.description}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
