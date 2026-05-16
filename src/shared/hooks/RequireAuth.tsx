6831ac35fc0f596b0c239a05431f833bbdae1e83
azan-personal <112457939+azanra@users.noreply.github.com>
2026-05-16 18:33:00 +0700

    feat: add loading state for the authentication

--- src/shared/hooks/RequireAuth.tsx
+++ src/shared/hooks/RequireAuth.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router";
 import { useAuth } from "./useAuth";
 import type { JSX } from "react";
+import LoadingPage from "../components/LoadingPage";
 
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const { user, loading } = useAuth();
 
-  if (loading) return <div className="p-4">Loading...</div>;
+  if (loading) return <LoadingPage />;
   if (!user) return <Navigate to="/" replace />;
 
   return children;
