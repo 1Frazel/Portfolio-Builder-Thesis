37d1e17ee954081153af6977ade8d955e0eb1361
azan-personal <112457939+azanra@users.noreply.github.com>
2026-05-17 15:00:30 +0700

    feat: separate the directory based of the page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,9 +2,10 @@ import App from "../App";
 import ErrorPage from "../pages/error/ErrorPage";
 import AtsTemplate from "../pages/generate-pdf/components/templates/ats-template/AtsTemplate";
 import ParsePdf from "../pages/parse-pdf/ParsePdf";
-import CVList from "../pages/generation/CVList";
-import TemplateSelection from "../pages/generation/TemplateSelection";
+
 import RequireAuth from "../shared/hooks/RequireAuth";
+import TemplateSelection from "../pages/select-template/TemplateSelection";
+import CVList from "../pages/my-resume/CVList";
 
 const routes = [
   {
